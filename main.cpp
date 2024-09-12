3613da735b04167464e3e5abe84a71940eed4ed7
Emil Mammadli <56939463+pncln@users.noreply.github.com>
2024-09-12 10:34:30 +0400

    Update main.cpp
    Removed Mongo driver

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include <QApplication>
 #include "mainwindow.h"
-#include <mongocxx/client.hpp>
-#include <mongocxx/instance.hpp>
-#include <mongocxx/uri.hpp>
 #include <bsoncxx/json.hpp>
 #include <QDebug>
 
 int main(int argc, char *argv[]) {
-    // Initialize the MongoDB driver
-    mongocxx::instance instance{};
 
     QApplication app(argc, argv);
 
